85ad19536aaae97654a98460f0e2eba41c835c44
haleydek <hcd09@live.com>
2019-09-24 15:48:13 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,54 @@ class App extends React.Component {
   constructor() {
     super()
 
+    this.baseURL = '/api/pets'
+
     this.state = {
       pets: [],
       filters: {
         type: 'all'
       }
     }
+
+    this.handleFilters = this.handleFilters.bind(this)
+    this.getPetsURL = this.getPetsURL.bind(this)
+    this.handleFindPets = this.handleFindPets.bind(this)
+    this.handleAdoption = this.handleAdoption.bind(this)
+  }
+
+  handleFilters(newType){
+    this.setState({
+      ...this.state, filters: { type: newType }
+    })
+  }
+
+  getPetsURL(){
+    if (this.state.filters.type === "all"){
+      return this.baseURL
+    } else if (this.state.filters.type === "cat" || "dog" || "micropig"){
+      return `${this.baseURL}?type=${this.state.filters.type}`
+    } else {
+      return false
+    }
+  }
+
+  handleFindPets(event){
+
+    console.log("filters state: " + this.state.filters)
+
+    if (this.getPetsURL()) {
+      fetch(this.getPetsURL())
+      .then(res => res.json())
+      .then(petsData => this.setState({...this.state, pets: petsData}, () => console.log(this.state)))
+    }
+  }
+
+  handleAdoption(petId){
+    let petsArray = this.state.pets.map(pet => {
+      return pet.id === petId ? {...pet, isAdopted: true} : pet;
+    })
+    
+    this.setState({pets: [...petsArray]}, () => console.log("pets state: " + this.state.pets))
   }
 
   render() {
@@ -24,10 +66,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.handleFilters} onFindPetsClick={this.handleFindPets} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.handleAdoption} />
             </div>
           </div>
         </div>
